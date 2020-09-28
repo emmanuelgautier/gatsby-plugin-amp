dd11cbedf48f80e8f9a7178691d86867c49f3ab7
Andrey Nikonov <a.einomi@gmail.com>
2020-09-28 15:20:21 +0300

    Fix canonical urls

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -318,12 +318,16 @@ export const replaceRenderer = (
       });
       iframe.parentNode.replaceChild(ampIframe, iframe);
     });
+
     const styleTags = [].slice.call(document.getElementsByTagName("style"));
     styleTags.forEach(styleTag => {
       if (styleTag.getAttribute('data-emotion-css') != null) {
           styleTag.setAttribute('amp-custom', "");
       }
     })
+
+          const iframes = [].slice.call(document.getElementsByTagName("iframe"));
+
     setHeadComponents(
       Array.from(new Set(headComponents)).map((component, i) => (
         <Fragment key={`head-components-${i}`}>
